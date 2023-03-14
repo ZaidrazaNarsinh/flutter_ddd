fb6c10799d7d875798184f33857a2a22a7e4a04e
Zaidraza Narsinh <zaidraza.narsinh@imperoit.com>
2023-03-14 17:39:43 +0530

    email and password validation

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,1 +1,22 @@
+import 'package:flutter_ddd/domain/auth/value_objects.dart';
+import 'package:flutter_ddd/domain/core/failures.dart';
+import 'package:dartz/dartz.dart';
 
+abstract class ValueObjects<T> {
+  Either<ValueFailure<T>, T> get value;
+
+  const ValueObjects();
+
+  @override
+  bool operator ==(covariant EmailAddress other) {
+    if (identical(this, other)) return true;
+
+    return other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)'; 
+}
